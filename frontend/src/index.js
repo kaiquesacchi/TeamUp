d359bf1533f9b76cf97445b6aa26671639a59148
Kaique Sacchi <sacchikaique@gmail.com>
2018-09-25 19:02:46 -0300

    template da dashboard adicionado, conflitos iniciais resolvidos, novos modulos do npm

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 
-import App from './App';
+import { BrowserRouter, Route, Link, NavLink, Switch } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import 'assets/css/index.css';
+import 'assets/css/App.css';
+
+import RegisterDemand from './pages/Demands/RegisterDemand';
+import Authentication from './pages/Authentication/Authentication';
+import Home from './pages/Home/Home';
+
+import indexRoutes from "routes/index.jsx";
+
+
+ReactDOM.render(
+
+<BrowserRouter>
+	<Switch>
+		<Route exact path="/" component={Home}>
+		</Route>
+		<Route path="/auth" component={Authentication}>
+		</Route>
+		<Route path="/demand" component={RegisterDemand}>
+		</Route>
+	</Switch>
+</BrowserRouter>,
+
+document.getElementById('root'));
