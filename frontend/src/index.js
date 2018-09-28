3752dae4160dc704ebdfe50b581df9a7e9428c56
Kaique Sacchi <sacchikaique@gmail.com>
2018-09-27 22:38:07 -0300

    adicionado import do css do material-design, organizacao de arquivos

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import 'assets/css/index.css';
 import 'assets/css/Intro.css';
+import "assets/css/material-dashboard-react.css?v=1.5.0";
 
 import indexRoutes from "routes/index.jsx";
 import Home from "views/Home/Home.js"
