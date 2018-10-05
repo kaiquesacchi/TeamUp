9fa32383653a4de74610508f6cb45041e46f9f69
Kaique Sacchi <sacchikaique@gmail.com>
2018-10-04 23:04:07 -0300

    adicionado componente para botao com imagens. botao em completar-sign-up funcionando

--- frontend/src/views/Authentication/CompleteSignUp.js
+++ frontend/src/views/Authentication/CompleteSignUp.js
@@ -5,95 +5,35 @@ import { withStyles } from '@material-ui/core/styles';
 import ButtonBase from '@material-ui/core/ButtonBase';
 import Typography from '@material-ui/core/Typography';
 
-const styles = theme => ({
-	root: {
-		display: 'flex',
-		flexWrap: 'wrap',
-		minWidth: 300,
-		width: '100%',
-	},
-	image: {
-		position: 'relative',
-		height: 200,
-		[theme.breakpoints.down('xs')]: {
-			width: '100% !important', // Overrides inline-style
-			height: 100,
-		},
-		'&:hover, &$focusVisible': {
-			zIndex: 1,
-			'& $imageBackdrop': {
-				opacity: 0.15,
-			},
-			'& $imageMarked': {
-				opacity: 0,
-			},
-			'& $imageTitle': {
-				border: '4px solid currentColor',
-			},
-		},
-	},
-	focusVisible: {},
-	imageButton: {
-		position: 'absolute',
-		left: 0,
-		right: 0,
-		top: 0,
-		bottom: 0,
-		display: 'flex',
-		alignItems: 'center',
-		justifyContent: 'center',
-		color: theme.palette.common.white,
-	},
-	imageSrc: {
-		position: 'absolute',
-		left: 0,
-		right: 0,
-		top: 0,
-		bottom: 0,
-		backgroundSize: 'cover',
-		backgroundPosition: 'center 40%',
-	},
-	imageBackdrop: {
-		position: 'absolute',
-		left: 0,
-		right: 0,
-		top: 0,
-		bottom: 0,
-		backgroundColor: theme.palette.common.black,
-		opacity: 0.4,
-		transition: theme.transitions.create('opacity'),
-	},
-	imageTitle: {
-		position: 'relative',
-		padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 4}px ${theme.spacing.unit + 6}px`,
-	},
-	imageMarked: {
-		height: 3,
-		width: 18,
-		backgroundColor: theme.palette.common.white,
-		position: 'absolute',
-		bottom: -2,
-		left: 'calc(50% - 9px)',
-		transition: theme.transitions.create('opacity'),
-	},
-});
-
-const images = [
-	{
-		url: '../../assets/img/CompleteSignUp/client.jpg',
-		title: 'Cliente',
-		width: '50%',
-	},
-	{
-		url: '../../assets/img/CompleteSignUp/developer.jpg',
-		title: 'Desenvolvedor',
-		width: '50%',
-	},
-];
-
+import ImageButtons from 'components/CustomButtons/ImageButtons.js'
 
 
 class CompleteSignUp extends Component {
+	constructor(props){
+		super(props)
+		this.state = {
+			images: [
+				{
+					url: 'client.jpg',
+					title: 'Cliente',
+					width: '50%',
+					onClick: function() {
+						props.history.push("/inicio")
+					}
+				},
+				{
+					url: '../../assets/img/CompleteSignUp/developer.jpg',
+					title: 'Desenvolvedor',
+					width: '50%',
+					onClick: function() {
+						props.history.push("/inicio")
+					}
+				}
+			]
+		};
+	}
+
+
 	render() {
 	const { classes } = this.props;
 		return (
@@ -105,38 +45,7 @@ class CompleteSignUp extends Component {
 		<h1>Seja bem vindo, Fernando</h1>
 		<h2>Já estamos terminando, só precisamos saber</h2>
 		<h2>com qual desses perfís você se identifica: </h2>
-		<div className={classes.root}>
-			{images.map(image => (
-				<ButtonBase
-					focusRipple
-					key={image.title}
-					className={classes.image}
-					focusVisibleClassName={classes.focusVisible}
-					style={{
-						width: image.width,
-					}}
-				>
-					<span
-						className={classes.imageSrc}
-						style={{
-							backgroundImage: `url(${image.url})`,
-						}}
-					/>
-					<span className={classes.imageBackdrop} />
-					<span className={classes.imageButton}>
-						<Typography
-							component="span"
-							variant="subheading"
-							color="inherit"
-							className={classes.imageTitle}
-						>
-							{image.title}
-							<span className={classes.imageMarked} />
-						</Typography>
-					</span>
-				</ButtonBase>
-			))}
-		</div>
+		<ImageButtons images={this.state.images} />
 	</div>
 </div>
 
@@ -144,4 +53,4 @@ class CompleteSignUp extends Component {
 	}
 }
 
-export default withStyles(styles)(CompleteSignUp);
+export default CompleteSignUp;
