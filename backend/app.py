95eb6613803444d86454aec273ff40780adc944a
Kaíque Sacchi <sacchikaique@gmail.com>
2018-11-30 16:42:20 -0200

    referencia ao banco de dados adicionada

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 
 from controllers.create_proposal import CreateProposal
 from controllers.start import Start
@@ -11,11 +12,26 @@ app = Flask(__name__)
 api = Api(app)
 CORS(app)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:@localhost/teamup'
+db = SQLAlchemy(app)
 
 api.add_resource(CreateProposal, '/proposta')
 api.add_resource(Start, '/start')
 api.add_resource(ProjectsList, '/projetos')
 api.add_resource(ConcludeProject, '/projeto/concluir')
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(30))
+
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = password
+
+db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True)
